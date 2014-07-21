147706ecb4d1304b7594561729fcd89b2bd328b8
Kevin Ho <keho98@gmail.com>
2014-07-20 23:19:26 -0700

    Implements list view with adapter

--- app/src/main/java/com/personal/kho/tenki/MainActivity.java
+++ app/src/main/java/com/personal/kho/tenki/MainActivity.java
@@ -10,7 +10,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
@@ -58,6 +64,26 @@ public class MainActivity extends Activity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            String[] forecastData = {
+                    "Mon 6/23 - Sunny - 31/17",
+                    "Tue 6/24 - Foggy - 21/8",
+                    "Wed 6/25 - Cloudy - 22/17",
+                    "Thurs 6/26 - Rainy - 18/11",
+                    "Fri 6/27 - Foggy - 21/10",
+                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                    "Sun 6/29 - Sunny - 20/7"
+            };
+            ArrayList<String> weatherForecasts =  new ArrayList<String>(Arrays.asList(forecastData));
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                    getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview,
+                    weatherForecasts);
+            ListView forecastText = (ListView) rootView.findViewById(R.id.listview_forecast);
+            forecastText.setAdapter(adapter);
+
             return rootView;
         }
     }
